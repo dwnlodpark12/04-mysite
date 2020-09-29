ea6375462d39adf75fd5dd9bb6bcf48a171abc25
downlodpark12 <downlodpark12@gmail.com>
2020-09-29 18:18:37 +0900

    집에가서 완성하기

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,15 @@
+/************* 네비게이션 **************/
 
+
+
+
+
+/************* .main-slide **************/
+
+
+
+
+/************* text 슬라이드 **************/
 $(window).scroll(function () {
 	var sctop = $(document).scrollTop();
 	if(sctop > 100) {
