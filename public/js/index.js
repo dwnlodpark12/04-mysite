7719f23ed2c42a0326d550a486c4b9d152fc67fc
dwnlodpark12 <dwnlodpark12@gmail.com>
2020-10-13 20:55:25 +0900

    집에 가자

--- public/js/index.js
+++ public/js/index.js
@@ -38,14 +38,62 @@ function onDarkMode() {
 		{id: 3, src: '../img/main1-4.jpg', title: 'Trendy Bags', categ:'Creative', price:'45.50€'}
 	];
 
-	var $slideStage = $(".main-right .stage");
 	var $slideWrap = $(".main-right .slide-wrap");
+	var $imgWrap = $(".main-right .img-wrap");
 	var $pagerWrap = $(".main-right .pager-wrap");
-	var $pager;				// 생성된 $(".wrapper6 .pager")
+	var $pager;				// 생성된 $(".main-right .pager")
 	var $slides = [];	// $(".slide")들 모두를 담아놓는 배열(필요할때 복사해서 가져다 쓴다)
 	var idx = 0;											// 현재 화면에 보이는 slide의 index
 	var lastIdx = slides.length - 1;	// $(".slide")들 중에 마지막 index
 	var interval;											// setInterval을 담아놓는 변수
+
+	function init() {
+		var html, i;
+		for (i in slides) {
+			html = '<div class="slide">';
+			html += '<img class="w-100" src="'+slides[i].src+'">';
+			html += '<div class="txt-wrap">'; 
+			html += '<div class="txt-l">';
+			html += '<h3 class="img-title">'+slides[i].title+'</h3>';
+			html += '<div class="img-categ">'+slides[i].categ+'</div>';
+			html += '</div>';
+			html += '<div class="img-price">'+slides[i].price+'</div>';
+			html += '</div>';
+			html += '</div>';
+			$slides[i] = $(html);
+			html = '<span class="pager">●</span>';
+			$pagerWrap.append(html);
+		}
+		$pager = $pagerWrap.find(".pager");
+		$pager.click(onPagerClick).eq(idx).addClass("active");
+		slideInit();
+		interval = setInterval(onNext, 3000);
+	}
+	function slideInit() {
+		$pagerWrap.html($slides[idx].clone());
+	}
+
+	$slideWrap.mouseover(onHover);
+	$slideWrap.mouseleave(onLeave);
+
+	function onHover() {
+		clearInterval(interval);
+	}
+
+	function onLeave() {
+		interval = setInterval(onNext, 3000);
+	}
+
+	function onPagerClick() {
+		idx = $(this).index();
+		ani();
+	}
+
+	function ani() {
+		$pager.removeClass("active").eq(idx).addClass("active");
+		$($slides[idx].clone()).appendTo($imgWrap).stop().animate({"opacity": 1}, 500, slideInit);
+	}
+	init();
 })();
 
 
