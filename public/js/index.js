9eb63d36cac66f47464e04dcfa2eb559d170d200
downlodpark12 <downlodpark12@gmail.com>
2020-10-14 16:19:50 +0900

    sctop 질문

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,4 @@
+
 /************* 네비게이션 **************/
 $("li.navi").mouseenter(onEnter);
 $("li.navi").mouseleave(onLeave);
@@ -30,7 +31,6 @@ function onDarkMode() {
 }
 
 /************* .main-slide **************/
-(function() {
 	var slides = [
 		{id: 0, src: '../img/main1-1.jpg', title: 'Red Watch', categ:'Digital', price:'45.50€'},
 		{id: 1, src: '../img/main1-2.jpg', title: 'Sweater Print', categ:'Urban', price:'45.50€'},
@@ -38,67 +38,12 @@ function onDarkMode() {
 		{id: 3, src: '../img/main1-4.jpg', title: 'Trendy Bags', categ:'Creative', price:'45.50€'}
 	];
 
-	var $slideWrap = $(".main-right .slide-wrap");
-	var $imgWrap = $(".main-right .img-wrap");
-	var $pagerWrap = $(".main-right .pager-wrap");
-	var $pager;				// 생성된 $(".main-right .pager")
-	var $slides = [];	// $(".slide")들 모두를 담아놓는 배열(필요할때 복사해서 가져다 쓴다)
-	var idx = 0;											// 현재 화면에 보이는 slide의 index
-	var lastIdx = slides.length - 1;	// $(".slide")들 중에 마지막 index
-	var interval;											// setInterval을 담아놓는 변수
 
-	function init() {
-		var html, i;
-		for (i in slides) {
-			html = '<div class="slide">';
-			html += '<img class="w-100" src="'+slides[i].src+'">';
-			html += '<div class="txt-wrap">'; 
-			html += '<div class="txt-l">';
-			html += '<h3 class="img-title">'+slides[i].title+'</h3>';
-			html += '<div class="img-categ">'+slides[i].categ+'</div>';
-			html += '</div>';
-			html += '<div class="img-price">'+slides[i].price+'</div>';
-			html += '</div>';
-			html += '</div>';
-			$slides[i] = $(html);
-			html = '<span class="pager">●</span>';
-			$pagerWrap.append(html);
-		}
-		$pager = $pagerWrap.find(".pager");
-		$pager.click(onPagerClick).eq(idx).addClass("active");
-		slideInit();
-		interval = setInterval(onNext, 3000);
-	}
-	function slideInit() {
-		$pagerWrap.html($slides[idx].clone());
-	}
-
-	$slideWrap.mouseover(onHover);
-	$slideWrap.mouseleave(onLeave);
-
-	function onHover() {
-		clearInterval(interval);
-	}
-
-	function onLeave() {
-		interval = setInterval(onNext, 3000);
-	}
-
-	function onPagerClick() {
-		idx = $(this).index();
-		ani();
-	}
-
-	function ani() {
-		$pager.removeClass("active").eq(idx).addClass("active");
-		$($slides[idx].clone()).appendTo($imgWrap).stop().animate({"opacity": 1}, 500, slideInit);
-	}
-	init();
-})();
 
 
 
 /************* text 슬라이드 **************/
+
 $(window).scroll(function () {
 	var sctop = $(document).scrollTop();
 	console.log(sctop);
@@ -107,28 +52,20 @@ $(window).scroll(function () {
 	}
 });
 
+/************* selection hover **************/
+
+$("li.list").mouseenter(listEnter);
+$("li.list").mouseleave(listLeave);
+
+function listEnter(){
+	$(this).find(".add-cart").stop().fadeIn(500);
+} 
+function listLeave(){
+	$(this).find(".add-cart").stop().fadeOut(500);
+} 
+
+
 /************* play mouseAni **************/
-/* $(".play-bgwrap").mouseomve(function(e) {
-	$("img.play").css("transform","translateX(e.pageX)");
-	$("img.play").css("transform","translateY(e.pageY)");
-});
- */
-/************* pop-hoverAni **************/
-
-$("li.pop").mouseenter(popEnter);
-$("li.pop").mouseleave(popLeave);
-
-function popEnter() {
-	$(this).find(".img-hover").css("display", "block");
-	$(this).find(".img-hover img").css("transform");
-	$(this).find(".img-hover img").css("transform", "scaleX(1)");
-}
-function popLeave() {
-	$(this).find(".img-hover img").css("transform", "scaleX(1.3)");
-	$(this).find(".img-hover").css("display", "none");
-}
-
-
 
 $(".play-wrap .play").mousemove(function(e){
 	var x = e.offsetX;
@@ -144,3 +81,37 @@ $(".play-wrap .play").mousemove(function(e){
 		$(this).css("transform", "translate(0, 0)");
 	}
 });
+
+
+
+/************* pop-hoverAni **************/
+
+$("li.pop").mouseenter(popEnter);
+$("li.pop").mouseleave(popLeave);
+
+function popEnter() {
+	$(this).find(".img-hover").css("display", "block");
+	$(this).find(".img-hover img").css("transform");
+	$(this).find(".img-hover img").css("transform", "scaleX(1)");
+}
+function popLeave() {
+	$(this).find(".img-hover img").css("transform", "scaleX(1.3)");
+	$(this).find(".img-hover").css("display", "none");
+}
+
+/************* back-to-top **************/
+
+$(window).scroll(function() {
+	if(sctop<=500) {
+		$(".back-top").fadeIn(200);
+	}
+	else {
+		$(".back-top").fadeOut(200);
+	}
+});
+
+
+$(".back-top").click(onTopClick);
+function onTopClick() {
+	$('html,body').animate({scrollTop},500);
+}
