2d3805f8e460e697c959deae29ebe362824304f0
dwnlodpark12 <dwnlodpark12@gmail.com>
2020-10-17 19:47:56 +0900

    slide 못풀겠다

--- public/js/ex.js
+++ public/js/ex.js
@@ -46,21 +46,91 @@ function onDarkMode() {
 }
 
 /************* .main-slide **************/
-
 (function(){
+var slides = [
+	{id: 0, src: '../img/main1-1.jpg', title: 'Red Watch', categ:'Digital', price:'45.50€'},
+	{id: 1, src: '../img/main1-2.jpg', title: 'Sweater Print', categ:'Urban', price:'45.50€'},
+	{id: 2, src: '../img/main1-3.jpg', title: 'Framed Poster', categ:'Vision', price:'45.50€'},
+	{id: 3, src: '../img/main1-4.jpg', title: 'Trendy Bags', categ:'Creative', price:'45.50€'}
+];
 
-  var $slideStage = $(".stage .stage");
-  var slideWrap = $(".stage .slide-wrap");
-  var txtWrap = $(".stage .txt-wrap");
-  var pagerWrap = $(".stage .pager-wrap");
-  var pager;
-  var slides = []; 
+
+  var $slideStage = $(".main-right .stage");
+  var $slideWrap = $(".stage .slide-wrap");
+  var $pagerWrap = $(".stage .pager-wrap");
+  var $pager;
+  var $slides = []; 
   var idx = 0; 
   var lastIdx = slides.length - 1; 
   var interval; 
 
 
   function init() {
+    var html,i;
+    for (i in slides) {
+      html = '<div class="slide">'
+      html += '<img class="w-100" src="'+slides[i].src+'">';
+      html += '<div class="txt-wrap">'
+      html += '<div class="txt-l">'
+      html += '<div class="txt-title">'+slides[i].title+'</div>';
+      html += '<div class="txt-categ">'+slides[i].categ+'</div>';
+      html += '</div>'; /* /.txt-l */
+      html += '<div class="txt-price">'+slides[i].price+'</div>';
+      html += '</div>';	/* /.txt-wrap */
+			html += '</div>';  /* /.slide */
+      $slides[i] = $(html);
+
+      html = '<span class="pager">●</span>'; 
+			 $pagerWrap.append(html); 
+			}
+
+		$pager = $pagerWrap.find(".pager");
+		$pager.click(onPagerClick).eq(idx).addClass("active");
+		slideInit();
+		interval = setInterval(onNext, 3000);
+	}
+
+	function slideInit() {
+		$slideWrap.html($slides[idx].clone());
+	}
+
+	$slideStage.mouseover(onHover);
+	$slideStage.mouseleave(onLeave);
+
+	function onHover() {
+		clearInterval(interval);
+	}
+	function onLeave() {
+		interval = setInterval(onNext, 3000);
+	}
+
+	function onPrev() {
+		target = '100%';
+		idx = (idx == 0) ? lastIdx : idx - 1;
+		ani();
+	}
+	function onNext() {
+		target = '-100%';
+		idx = (idx == lastIdx) ? 0 : idx + 1;
+		ani();
+	}
+
+	function onPagerClick() {
+		idx = $(this).index();
+		ani();
+	}
+
+	function ani() {
+		$pager.removeClass("active")
+		$pager.eq(idx).addClass("active");
+		$($slides[idx].clone()).appendTo($slideWrap).animate({"opacity":1},500, slideInit);
+	}
+	init();
+})(); 
+
+
+/* 
+ function init() {
     var html,i;
     for (i in slides) {
       html = '<div class="slide">'
@@ -81,7 +151,7 @@ function onDarkMode() {
     $pager = $pagerWrap.find(".pager");
     $pager.click(onPagerClick).eq(idx).addClass("active");
 		slideInit();
-	/* 	interval = setInterval(onNext, 3000); 
+		interval = setInterval(onNext, 3000); 
   }
 
   function slideInit() {
@@ -110,6 +180,7 @@ function onDarkMode() {
 	}
 	init();
 })(); 
- */
+ 
 
 
+*/
